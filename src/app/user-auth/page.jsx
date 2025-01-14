a9c1adebad6449fb721464d0c67e2c5a6f07a12b
MUSA DANLADI ISAH <datalinkscomputer@gmail.com>
2025-01-14 08:16:47 +0100

    Update page.jsx

--- src/app/user-auth/page.jsx
+++ src/app/user-auth/page.jsx
@@ -163,8 +163,6 @@ const page = () => {
         by offering real-time assistance about NASENI's initiatives and innovative products.
       </p>
       <Button
-            className="bg-gradient-to-r from-[#0066cc] via-[#004080] to-[#00264d] text-white font-semibold py-1 px-3 md:py-2 md:px-4 rounded-full shadow-lg text-xs md:text-sm hover:scale-105 transform"
-            onClick={handleLogin}
       >
         Launch Chatbot
       </Button>
